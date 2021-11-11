0862b52ae63f76d89695f983d1874abe973e4f71
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2021-11-10 19:59:14 -0500

    all done

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ Car.prototype.fill = function(gallons){
 }
 
 Car.prototype.run = function(km){
-  this.odometer = km
+  
 }
 
 const caravan = new Car('BMW', 40);
@@ -125,10 +125,17 @@ console.log(caravan);
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+    this.name = name;
+    this.age= age;
+    this.favoriteToy = favoriteToy;
+    this.stomach = [];
 }
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
